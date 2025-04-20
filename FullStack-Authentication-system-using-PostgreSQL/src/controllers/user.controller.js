5242bcda1e0c23631a07ebe7c5225e10b6753379
Biki Dey <63406490+Biki721@users.noreply.github.com>
2025-04-20 18:13:33 +0000

    add prisma

--- FullStack-Authentication-system-using-PostgreSQL/src/controllers/user.controller.js
+++ FullStack-Authentication-system-using-PostgreSQL/src/controllers/user.controller.js
@@ -1,5 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {PrismaClient} from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export const registerUser = asyncHandler(async (req, res, next) => {
-  res.send("Registration successful");
+  const {name, email, password} = req.body
+
+  if( [name,email, password].some((field)=> field.trim()==="")){
+    throw new ApiError(400,"All fields are required");
+  }
+
+  
+
 });
