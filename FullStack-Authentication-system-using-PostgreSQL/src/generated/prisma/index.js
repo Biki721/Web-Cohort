6901ee76fb196afe7011ec00060c72a6e2c0c34b
Biki Dey <63406490+Biki721@users.noreply.github.com>
2025-04-21 14:09:59 +0000

    add prisma generator to intercat with database

--- FullStack-Authentication-system-using-PostgreSQL/src/generated/prisma/index.js
+++ FullStack-Authentication-system-using-PostgreSQL/src/generated/prisma/index.js
@@ -165,11 +165,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_zwmxrkh6Qi3I@ep-lucky-bar-a495lsw1-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+        "value": null
       }
     }
   },
